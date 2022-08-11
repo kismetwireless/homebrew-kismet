4f1758a781821e9f0ceb07402c8e3104e8dc22ad
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-08-10 22:38:30 -0400

    brew: Initial Kismet tap

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kismet Homebrew Formulae
+
+Basic formula files for Homebrew on macOS
+
+To install:
+
+1. Install Homebrew 
+2. brew tap kismetwireless/kismet 
+3. brew install kismet
---
+++ kismet.rb
@@ -0,0 +1,34 @@
+# Kismet release Formula
+# 
+# Does not build python modules - need to make custom packages for them
+#
+# Does not install suidroot
+
+class Kismet < Formula
+  desc "Kismet Wi-Fi and Wireless Sniffer"
+  homepage "https://www.kismetwireless.net"
+  url "https://github.com/kismetwireless/kismet/archive/refs/tags/kismet-2022-08-R1.zip"
+  sha256 "0ebffeff643e0bbf9875f2b3e5d34efad9f9d9a613f27db4c0cc4e675ef57aa7"
+  license "GPL-2"
+
+  depends_on "pkg-config"
+  # depends_on "python3"
+  depends_on "libpcap"
+  depends_on "protobuf"
+  depends_on "protobuf-c"
+  depends_on "pcre"
+  depends_on "librtlsdr"
+  depends_on "libbtbb"
+  depends_on "ubertooth" 
+  depends_on "libusb" 
+  depends_on "openssl" 
+  depends_on "libwebsockets"
+
+  def install
+    ENV.append "CPPFLAGS", "-I#{Formula["openssl"].include}"
+    ENV.append "INSTUSR", "${USER}"
+    ENV.append "INSTGRP", "staff"
+    system "./configure", *std_configure_args, "--disable-silent-rules", "--enable-wifi-coconut", "--disable-python-tools"
+    system "make", "install"
+  end
+end
